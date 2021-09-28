95c498ca36a1c6bdb6de39870295891b6f537f98
zionli(李正) <zionli@webank.com>
2021-09-28 20:11:35 +0800

    initial

--- src/main/java/com/example/Redis/controller/RedisController.java
+++ src/main/java/com/example/Redis/controller/RedisController.java
@@ -1,5 +1,7 @@
 package com.example.Redis.controller;
 
+import com.example.Redis.utils.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,8 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class RedisController {
 
+    @Autowired
+    private RedisUtil redisUtil;
 
     @GetMapping("/hello")
     public ModelAndView hello() {
@@ -18,8 +22,12 @@ public class RedisController {
     public ModelAndView setRedis(@RequestParam(value = "redisKey", defaultValue = "") String redisKey, @RequestParam(value = "redisValue", defaultValue = "") String redisValue) {
         System.out.println(redisKey + " : " + redisValue);
 
+        redisUtil.set(redisKey, redisValue);
+
         return new ModelAndView("/redis");
     }
+
+
 
 
 }
