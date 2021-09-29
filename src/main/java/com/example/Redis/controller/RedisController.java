77bedf25273c84c777f0ac4b75cc2d5a6fca046b
zionli(李正) <zionli@webank.com>
2021-09-29 10:58:26 +0800

    增加读redis的controller方法

--- src/main/java/com/example/Redis/controller/RedisController.java
+++ src/main/java/com/example/Redis/controller/RedisController.java
@@ -1,14 +1,16 @@
 package com.example.Redis.controller;
 
+import com.example.Redis.service.MyLogger;
 import com.example.Redis.utils.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class RedisController {
+
+    @Autowired
+    private MyLogger myLogger;
 
     @Autowired
     private RedisUtil redisUtil;
@@ -20,11 +22,30 @@ public class RedisController {
 
     @RequestMapping("/set")
     public ModelAndView setRedis(@RequestParam(value = "redisKey", defaultValue = "") String redisKey, @RequestParam(value = "redisValue", defaultValue = "") String redisValue) {
-        System.out.println(redisKey + " : " + redisValue);
-
-        redisUtil.set(redisKey, redisValue);
+        try {
+            redisUtil.set(redisKey, redisValue);
+            myLogger.info("redis写数据："+ redisKey + "  " + redisValue);
+        } catch (Exception e) {
+            myLogger.error("redis写数据失败："+ redisKey + "  " + redisValue);
+        }
 
         return new ModelAndView("/redis");
+    }
+
+    @RequestMapping("/get")
+    public String getRedis(@RequestParam(value = "redisKey", defaultValue = "") String redisKey) {
+        String redisValue = null;
+        try {
+            redisValue = (String)redisUtil.get(redisKey);
+            myLogger.info("redis读数据："+ redisKey + "  " + redisValue);
+        } catch (Exception e) {
+            myLogger.error("redis读数据失败："+ redisKey);
+        }
+        if (redisValue != null) {
+            return redisValue;
+        }
+
+        return "fail to read";
     }
 
 
---
+++ src/main/java/com/example/Redis/service/MyLogger.java
@@ -0,0 +1,313 @@
+package com.example.Redis.service;
+
+import org.slf4j.Logger;
+import org.slf4j.Marker;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyLogger implements Logger {
+    @Override
+    public  String getName() {
+        return MyLogger.class.getName();
+    }
+
+    @Override
+    public boolean isTraceEnabled() {
+        return false;
+    }
+
+    @Override
+    public void trace(String s) {
+
+    }
+
+    @Override
+    public void trace(String s, Object o) {
+
+    }
+
+    @Override
+    public void trace(String s, Object o, Object o1) {
+
+    }
+
+    @Override
+    public void trace(String s, Object... objects) {
+
+    }
+
+    @Override
+    public void trace(String s, Throwable throwable) {
+
+    }
+
+    @Override
+    public boolean isTraceEnabled(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void trace(Marker marker, String s) {
+
+    }
+
+    @Override
+    public void trace(Marker marker, String s, Object o) {
+
+    }
+
+    @Override
+    public void trace(Marker marker, String s, Object o, Object o1) {
+
+    }
+
+    @Override
+    public void trace(Marker marker, String s, Object... objects) {
+
+    }
+
+    @Override
+    public void trace(Marker marker, String s, Throwable throwable) {
+
+    }
+
+    @Override
+    public boolean isDebugEnabled() {
+        return false;
+    }
+
+    @Override
+    public void debug(String s) {
+        System.out.println("[debug] " + s);
+    }
+
+    @Override
+    public void debug(String s, Object o) {
+
+    }
+
+    @Override
+    public void debug(String s, Object o, Object o1) {
+
+    }
+
+    @Override
+    public void debug(String s, Object... objects) {
+
+    }
+
+    @Override
+    public void debug(String s, Throwable throwable) {
+
+    }
+
+    @Override
+    public boolean isDebugEnabled(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void debug(Marker marker, String s) {
+
+    }
+
+    @Override
+    public void debug(Marker marker, String s, Object o) {
+
+    }
+
+    @Override
+    public void debug(Marker marker, String s, Object o, Object o1) {
+
+    }
+
+    @Override
+    public void debug(Marker marker, String s, Object... objects) {
+
+    }
+
+    @Override
+    public void debug(Marker marker, String s, Throwable throwable) {
+
+    }
+
+    @Override
+    public boolean isInfoEnabled() {
+        return true;
+    }
+
+    @Override
+    public void info(String s) {
+        System.out.println("[info] " + s);
+    }
+
+    @Override
+    public void info(String s, Object o) {
+
+    }
+
+    @Override
+    public void info(String s, Object o, Object o1) {
+
+    }
+
+    @Override
+    public void info(String s, Object... objects) {
+
+    }
+
+    @Override
+    public void info(String s, Throwable throwable) {
+
+    }
+
+    @Override
+    public boolean isInfoEnabled(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void info(Marker marker, String s) {
+
+    }
+
+    @Override
+    public void info(Marker marker, String s, Object o) {
+
+    }
+
+    @Override
+    public void info(Marker marker, String s, Object o, Object o1) {
+
+    }
+
+    @Override
+    public void info(Marker marker, String s, Object... objects) {
+
+    }
+
+    @Override
+    public void info(Marker marker, String s, Throwable throwable) {
+
+    }
+
+    @Override
+    public boolean isWarnEnabled() {
+        return true;
+    }
+
+    @Override
+    public void warn(String s) {
+        System.out.println("[warn] " + s);
+    }
+
+    @Override
+    public void warn(String s, Object o) {
+
+    }
+
+    @Override
+    public void warn(String s, Object... objects) {
+
+    }
+
+    @Override
+    public void warn(String s, Object o, Object o1) {
+
+    }
+
+    @Override
+    public void warn(String s, Throwable throwable) {
+
+    }
+
+    @Override
+    public boolean isWarnEnabled(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void warn(Marker marker, String s) {
+
+    }
+
+    @Override
+    public void warn(Marker marker, String s, Object o) {
+
+    }
+
+    @Override
+    public void warn(Marker marker, String s, Object o, Object o1) {
+
+    }
+
+    @Override
+    public void warn(Marker marker, String s, Object... objects) {
+
+    }
+
+    @Override
+    public void warn(Marker marker, String s, Throwable throwable) {
+
+    }
+
+    @Override
+    public boolean isErrorEnabled() {
+        return true;
+    }
+
+    @Override
+    public void error(String s) {
+        System.out.println("[error] " + s);
+    }
+
+    @Override
+    public void error(String s, Object o) {
+
+    }
+
+    @Override
+    public void error(String s, Object o, Object o1) {
+
+    }
+
+    @Override
+    public void error(String s, Object... objects) {
+
+    }
+
+    @Override
+    public void error(String s, Throwable throwable) {
+
+    }
+
+    @Override
+    public boolean isErrorEnabled(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void error(Marker marker, String s) {
+
+    }
+
+    @Override
+    public void error(Marker marker, String s, Object o) {
+
+    }
+
+    @Override
+    public void error(Marker marker, String s, Object o, Object o1) {
+
+    }
+
+    @Override
+    public void error(Marker marker, String s, Object... objects) {
+
+    }
+
+    @Override
+    public void error(Marker marker, String s, Throwable throwable) {
+
+    }
+}
