a6e3e1fab46d305f6467416662131b051f070e09
zionli(李正) <zionli@webank.com>
2022-04-13 21:13:45 +0800

    通过AOP实现方法耗时计算

--- src/main/java/com/example/common/validator/PermissionValidator.java
+++ src/main/java/com/example/common/validator/PermissionValidator.java
@@ -4,9 +4,7 @@ import com.example.common.annotation.Permission;
 import com.example.enums.ExceptionEnum;
 import com.example.exception.UserRegisterException;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -14,6 +12,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
+import java.time.Instant;
 import java.util.Map;
 import java.util.Objects;
 
@@ -28,16 +27,21 @@ public class PermissionValidator {
     private static final int ADMIN = 2;
     private static final int USER = 4;
 
+    // 执行耗时
+    private static long startTimeMilli;
+    private static long endTimeMilli;
+
     // 切点
     @Pointcut("@annotation(com.example.common.annotation.Permission)")
     public void pointCut() {
 
     }
 
-    // 切面
-    @AfterReturning("pointCut()")
+    // 执行方法前
+    @Before("pointCut()")
     public void surfaceCut(JoinPoint joinPoint) throws UserRegisterException{
         System.out.println("进入权限判断");
+        startTimeMilli = Instant.now().toEpochMilli();
 
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Method method = methodSignature.getMethod();
@@ -80,4 +84,15 @@ public class PermissionValidator {
 
 
     }
+
+    // 执行方法后
+    @After("pointCut()")
+    public void afterProcess() {
+        endTimeMilli = Instant.now().toEpochMilli();
+        long elapsedTime = endTimeMilli - startTimeMilli;
+        System.out.println("总耗时（毫秒）：" + elapsedTime);
+
+    }
+
+
 }
